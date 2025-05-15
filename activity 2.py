e15fd69336055c71c78e6a95f3eac35a09478c87
DoraChumba <dorcaskipyegon@gmail.com>
2025-05-15 21:42:41 +0300

    Initial commit: Add superhero and activity 2 files

---
+++ activity 2.py
@@ -0,0 +1,27 @@
+# Base class
+class Vehicle:
+    def move(self):
+        print("The vehicle moves.")
+
+# Subclasses with different move() implementations
+class Car(Vehicle):
+    def move(self):
+        print("Driving on the road 🚗")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flying in the sky ✈️")
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sailing on water 🚤")
+
+class Bicycle(Vehicle):
+    def move(self):
+        print("Pedaling on the path 🚴")
+
+# Polymorphism 
+vehicles = [Car(), Plane(), Boat(), Bicycle()]
+
+for v in vehicles:
+    v.move()
